75e8635f347daa85d6a2043a5153ff79967d91f3
Mikko Ohtamaa <mikko@opensourcehacker.com>
2013-05-25 11:28:07 +0300

    Don't bail out on 64-bit Linux

--- Skype4Py/__init__.py
+++ Skype4Py/__init__.py
@@ -7,7 +7,7 @@ import sys
 
 is_64bits = sys.maxsize > 2**32
 
-if is_64bits:
+if is_64bits and sys.platform == "darwin":
     raise RuntimeError("Skype4Py currently works only on 32-bit architecture. On 64-bit platforms you need to run this application in 32-bit compatibility mode. Please see documentation and Github issue tracker for more details.")
 
 
